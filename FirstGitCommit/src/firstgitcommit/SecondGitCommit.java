d314be5f3c6f4134198e1832221162491be08710
s572573 <gopiamaraneni07@gmail.com>
2024-01-24 12:50:01 -0600

    Second Git Commit

---
+++ FirstGitCommit/src/firstgitcommit/SecondGitCommit.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package firstgitcommit;
+
+/**
+ *
+ * @author S572573
+ */
+public class SecondGitCommit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World. I am coming for you!");
+    }
+    
+}
